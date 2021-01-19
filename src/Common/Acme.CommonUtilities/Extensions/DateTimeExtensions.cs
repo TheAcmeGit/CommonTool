cac1bf3b66ccbbdf179254fa4019fc55c1d132df
KO <KO@DESKTOP-LNNLHCR>
2021-01-19 14:33:32 +0800

    添加项目文件。

---
+++ src/Common/Acme.CommonUtilities/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acme.CommonUtilities.Extensions
+{
+    /// <summary>
+    /// 时间转换拓展
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// 当前时区
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static long ConvertDataTimeToLong(this DateTime dt)
+        {
+            DateTime dtStart = new DateTime(1997, 1, 1).ToLocalTime();
+            dt = dt.ToLocalTime();
+            TimeSpan toNow = dt.Subtract(dtStart);
+            long timeStamp = toNow.Ticks;
+            // 1Ticks = 100纳秒
+            //转换成毫秒
+            timeStamp = timeStamp / 10000;
+            return timeStamp;
+        }
+        /// <summary>
+        /// 时间戳转时间
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static DateTime ConvertLongToDateTime(this long d)
+        {
+            DateTime dtStart = new DateTime(1997, 1, 1).ToLocalTime();
+            long lTime = d*10000;
+            TimeSpan toNow = new TimeSpan(lTime);
+            DateTime dtResult = dtStart.Add(toNow);
+            return dtResult;
+        }
+    }
+}
---
+++ src/Common/Acme.CommonUtilities/Extensions/JsonExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Acme.CommonUtilities.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static string SerializerJson<T>(this T t)
+        {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+            MemoryStream ms = new MemoryStream();
+            ser.WriteObject(ms, t);
+            string jsonString = Encoding.UTF8.GetString(ms.ToArray());
+            ms.Close();
+            return jsonString;
+        }
+
+        /// <summary>  
+        /// JSON反序列化  
+        /// </summary>  
+        public static T DeserializeJson<T>(this string jsonString)
+        {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+            T obj = (T)ser.ReadObject(ms);
+            ms.Close();
+            return obj;
+        }
+    }
+}
